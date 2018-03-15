a3dc300c76306670ebc74c686a8f2eff36fb886b
Grasuggan <32665572+Grasuggan@users.noreply.github.com>
2018-03-15 13:39:37 +0100

    added ui

--- BookShop/UI/Program.cs
+++ BookShop/UI/Program.cs
@@ -13,6 +13,28 @@ namespace UI
 
         static void Main(string[] args)
         {
+            //AddBook();
+            AddBooks();
+        }
+
+        private static void AddBooks()
+        {
+            Book newBook1 = new Book { Title = "Hej hej Karlsson", ReleaseDate = DateTime.Now };
+            Book newBook2 = new Book { Title = "Fint Folk i Finkan 3", ReleaseDate = DateTime.Now };
+            List<Book> newBooks = new List<Book> { newBook1, newBook2 };
+            _context.Books.AddRange(newBooks);
+            Console.WriteLine(newBooks);
+            _context.SaveChanges();
+        }
+
+        private static void AddBook()
+        {
+            Book newBook = new Book();
+            newBook.Title = "Svindlande höjder";
+            newBook.ReleaseDate = System.DateTime.Now;
+
+            _context.Books.Add(newBook);
+            _context.SaveChanges();
         }
     }
 }
